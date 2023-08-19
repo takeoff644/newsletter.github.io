ca9f0da43ced5519df61f91bb7fe5eca76359aba
takeoff644 <141822805+takeoff644@users.noreply.github.com>
2023-08-19 14:36:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,16 @@
 const submit = document.getElementById("submit");
 const error= document.getElementById("error");
 const email= document.getElementById("Email"); 
-const val =  email.value;
 const format= /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
 function validate(){
-    if (format.test(val)){
+    if (format.test(email.value)){
+        return
         error.style.display="none";
         window.open("newsletter success.html", '_blank');
     } 
-    else{     
+    else{  
+        return
         email.style.border ="0.3em solid red";
         error.style.display="block";  
     }
